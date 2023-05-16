acfcd2443101436eb08d23f122379032a7c7125e
tranthekhiem14112001 <trankhiem99999@gmail.com>
2023-05-16 09:53:07 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    fs.readFile('./templates/index.html', 'utf-8', (err, str) => {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end('404 Not Found');
+        }
+        let username = 'Admin';
+        str = str.replace('{username}', username);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(str);
+        return res.end();
+    });
+});
+
+server.listen(8080, () => {
+    console.log(`Server running in http://localhost:8080`);
+});
